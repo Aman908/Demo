8c98b80431a875f58df19cc09232a9e754b5f150
91797 <91797@DESKTOP-2IJO1SV>
2019-10-18 20:12:05 +0530

    successful two

--- PageObjectModel/src/com/abc/Magento/MagentoTest.java
+++ PageObjectModel/src/com/abc/Magento/MagentoTest.java
@@ -3,7 +3,7 @@ package com.abc.Magento;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.abc.MagentoObjects.Login;
 import com.abc.MagentoObjects.Main;
@@ -14,7 +14,7 @@ public class MagentoTest {
 	public static void main(String[] args) 
 	{
 		String url="https://www.magento.com";
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
